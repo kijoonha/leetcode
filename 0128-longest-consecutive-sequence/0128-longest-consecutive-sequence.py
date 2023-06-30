bf4e9c03ded283c0900ef92258a90059d4cddee3
kijoonha <45222206+kijoonha@users.noreply.github.com>
2023-06-30 18:57:53 +0900

    Time: 662 ms (46.60%), Space: 46.6 MB (5.07%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -7,13 +7,13 @@ class Solution:
         aa = np.sort(nums)
         nums1 = aa[:-1]
         nums2 = aa[1:]
-        diff = np.array(nums2) - np.array(nums1)
+        #diff = np.array(nums2) - np.array(nums1)
         mm = 0
         mmm = 0
-        for ii in range(len(diff)):
-            if (diff[ii]==1):
+        for ii in range(len(nums2)):
+            if (nums2[ii]-nums1[ii]==1):
                 mm = mm+1
-            elif (diff[ii]==0):
+            elif (nums2[ii]-nums1[ii]==0):
                 continue
             else:
                 mm = 0
