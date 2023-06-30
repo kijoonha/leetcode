2e8ab45a42e0bb32e1f3491289c5ad6b7b460c4b
kijoonha <45222206+kijoonha@users.noreply.github.com>
2023-06-30 22:26:10 +0900

    Time: 61 ms (19.61%), Space: 20.9 MB (33.21%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
+        map<string, vector<string>> myMap;  
+        vector<vector<string>> results;  
 
+        for (int i=0; i<strs.size(); i++){
+            string sorted = strs[i];
+            sort(sorted.begin(),sorted.end());
 
-    map<string, vector<string>> myMap;  
-    vector<vector<string>> results;  
-    
-    for (int i=0; i<strs.size(); i++){
-        string sorted = strs[i];
-        sort(sorted.begin(),sorted.end());
-
-        if (myMap.count(sorted) ==0){
-            myMap.insert(pair<string, vector<string>> (sorted, {strs[i]}));
-        } 
-        else {
-            myMap[sorted].push_back(strs[i]);
-        }
+            if (myMap.count(sorted) ==0){
+                myMap.insert(pair<string, vector<string>> (sorted, {strs[i]}));
+            } 
+            else {
+                myMap[sorted].push_back(strs[i]);
+            }
         }  
-    for (auto it=myMap.begin(); it!=myMap.end();it++){
-        results.push_back(it->second);
-    }
-        
- 
-
-    return results;
-}    
+        for (auto it=myMap.begin(); it!=myMap.end();it++){
+            results.push_back(it->second);
+        }
+        return results;
+    }    
 };
