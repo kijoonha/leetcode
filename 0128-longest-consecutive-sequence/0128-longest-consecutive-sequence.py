f5d595da74d89180519d2bf9f279e5549677c31a
kijoonha <45222206+kijoonha@users.noreply.github.com>
2023-06-30 18:55:55 +0900

    Time: 647 ms (46.82%), Space: 47.7 MB (5.07%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums)==0:
+            return 0
+        aa = np.sort(nums)
+        nums1 = aa[:-1]
+        nums2 = aa[1:]
+        diff = np.array(nums2) - np.array(nums1)
+        mm = 0
+        mmm = 0
+        for ii in range(len(diff)):
+            if (diff[ii]==1):
+                mm = mm+1
+            elif (diff[ii]==0):
+                mm = mm
+            else:
+                mm = 0
+            print(mm)
+            if mm > mmm:
+                mmm = mm
+        return mmm+1
