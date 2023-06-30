59e8032f065f4b0dc1d8fa29f720fa84d939087d
kijoonha <45222206+kijoonha@users.noreply.github.com>
2023-06-30 22:12:51 +0900

    Time: 720 ms (19.73%), Space: 68.8 MB (19.69%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        set<int> sortedNums;
+        for (int i=0; i<nums.size(); i++){
+            sortedNums.insert(nums[i]);
+        }       
+        int cnt = 0;
+        int mcnt = 0;
+        set<int>::iterator it;
+        int preval = 0;
+        
+        //[]비어있을 경우
+        if (nums.empty()){
+            cout<<nums.size()<<"<비어있을때";
+            return (int)0;}
+        
+        for (it=sortedNums.begin(); it!=sortedNums.end(); it++){
+            //첫번째 값의 경우
+            if (it==sortedNums.begin()){
+                preval = *it;
+                mcnt = cnt;
+                cout<<"  cnt1: "<<cnt;
+            }
+            //2번째 값부터 앞의 값과 비교
+            else{
+                if (*it==preval+1){
+
+                    cnt++;
+                    if (mcnt<cnt)
+                        {mcnt = cnt;}
+                    cout<<"cnt2: "<<cnt;
+                    cout<<"  val: "<<*it;
+                }
+                else{
+                    cnt = 0;
+                }
+                preval = *it;
+            }  
+        }
+        mcnt++;
+        return mcnt;
+    }
+};
